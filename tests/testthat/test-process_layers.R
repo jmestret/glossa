2a739e2ae23a667c9c400fddef06fc6c4cbd486f
jmestret <jorgemartineztomas@gmail.com>
2024-09-13 11:47:24 +0200

    add terra to suggest

--- tests/testthat/test-process_layers.R
+++ tests/testthat/test-process_layers.R
@@ -33,17 +33,19 @@ test_that("layer_mask throws error for non-SpatRaster input", {
 })
 
 test_that("layer_mask throws error for non-sf polygon input", {
-  non_sf_polygon <- vect(matrix(c(-123, 36, -121, 36, -121, 38, -123, 38, -123, 36), ncol = 2, byrow = TRUE), type = "polygons")
+  skip_if_not_installed("terra")
+  non_sf_polygon <- terra::vect(matrix(c(-123, 36, -121, 36, -121, 38, -123, 38, -123, 36), ncol = 2, byrow = TRUE), type = "polygons")
   expect_error(layer_mask(toy_raster, non_sf_polygon), "Argument 'study_area' must be an 'sf' object.")
 })
 
 test_that("layer_mask processes raster correctly within polygon", {
+  skip_if_not_installed("terra")
   masked_layers <- layer_mask(toy_raster, toy_sf_polygon)
 
   # Check the number of layers remains the same
-  expect_equal(nlyr(masked_layers), 1)  # Should have 1 layer
+  expect_equal(terra::nlyr(masked_layers), 1)  # Should have 1 layer
 
   # Check if the extent of masked layers is within the polygon extent
-  masked_extent <- ext(masked_layers)
+  masked_extent <- terra::ext(masked_layers)
   expect_true(all(masked_extent[1:2] >= -123 & masked_extent[3:4] <= 38))  # Should be within the polygon bounds
 })
